c43c9d3fc3dc424b9ee9cfc8b22b7b9feeca56fb
Gustavo Pereira <gugarosp@gmail.com>
2025-06-19 14:49:17 -0300

    Zephyr configs

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { withZephyr } from 'vite-plugin-zephyr';
+import { withZephyr } from 'vite-plugin-zephyr';
 import federation from '@originjs/vite-plugin-federation';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
+        withZephyr(),
         federation({
             name: 'host_app',
             remotes: {
-                remoteApp: 'https://gustavo-pereira-14-module-federation-remote-modul-04682e89d-ze.zephyrcloud.app/assets/remoteEntry.js'
+                remoteApp: 'https://gustavo-pereira-14-module-federation-remote-modul-04682e89d-ze.zephyrcloud.app/assets/remoteEntry.js',
+                //remoteApp: 'http://localhost:4173/assets/remoteEntry.js'
             }
         })
     ],
